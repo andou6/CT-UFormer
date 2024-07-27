677d7a9987b5c903b9b26936861a47f7272a00b8
andou6 <133963958+andou6@users.noreply.github.com>
2024-07-27 21:28:12 +0800

    Add files via upload

---
+++ CT-UFormer/2A_train_injector.py
@@ -0,0 +1,8 @@
+from procedures.trainer import *
+
+
+print("Training CT-GAN Injector...")
+CTGAN_inj = Trainer(isInjector = True)
+#CTGAN_inj.train(epochs=200, batch_size=32, sample_interval=50)
+CTGAN_inj.train(epochs=150, batch_size=1, sample_interval=50)
+print('Done.')
